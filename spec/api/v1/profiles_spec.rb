7ba9f69504d06478b83f5f213a02bc2fbef6f894
aamnesia <lyapunova.anya@mail.ru>
2022-07-30 01:16:35 +0300

    all profiles except me

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -5,16 +5,17 @@ describe 'Profiles API', type: :request do
                      "ACCEPT" => 'application/json' } }
 
   describe 'GET /api/v1/profiles/me' do
+    let(:api_path) { '/api/v1/profiles/me' }
+
     it_behaves_like 'API Authorizable' do
       let(:method) { :get }
-      let(:api_path) { '/api/v1/profiles/me' }
     end
 
     context 'authorized' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-      before { get '/api/v1/profiles/me', params: { access_token: access_token.token }, headers: headers }
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
 
       it 'returns 200 status' do
         expect(response).to be_successful
@@ -29,6 +30,43 @@ describe 'Profiles API', type: :request do
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
           expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles/me' do
+    let(:api_path) { '/api/v1/profiles' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:users) { create_list(:user, 4) }
+      let(:me) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns all profiles except me' do
+        expect(json.size).to eq(users.size - 1)
+        expect(json).to_not include me
+      end
+
+      it 'returns all public fields' do
+        %w[id email admin created_at updated_at].each do |attr|
+          expect(json.last[attr]).to eq users.last.send(attr).as_json
+        end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(json.last).to_not have_key(attr)
         end
       end
     end
