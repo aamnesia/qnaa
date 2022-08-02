fe37e0048c3cd67398c6416e2df1e290558fb114
aamnesia <lyapunova.anya@mail.ru>
2022-08-02 15:05:13 +0300

    update daily digest to send new questions

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -15,5 +15,13 @@ FactoryBot.define do
     trait :with_attachment do
       files { fixture_file_upload(Rails.root.join('spec', 'rails_helper.rb'), 'rails_helper.rb') }
     end
+
+    trait :created_at_yesterday do
+      created_at { Date.yesterday }
+    end
+
+    trait :created_at_more_yesterday do
+      created_at { Date.today - 2 }
+    end
   end
 end
--- spec/mailers/daily_digest_spec.rb
+++ spec/mailers/daily_digest_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe DailyDigestMailer, type: :mailer do
   describe "digest" do
     let(:user) { create(:user)}
     let(:mail) { DailyDigestMailer.digest(user) }
+    let(:yesterday_questions) { create_list(:question, 2, :created_at_yesterday) }
+    let(:earlier_questions) { create_list(:question, 2, :created_at_more_yesterday) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Digest")
@@ -11,8 +13,16 @@ RSpec.describe DailyDigestMailer, type: :mailer do
       expect(mail.from).to eq(["from@example.com"])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "renders yesterday questions" do
+      yesterday_questions.each do |question|
+       expect(mail.body.encoded).to match question.title
+      end
+    end
+
+    it "doesnt render earlier questions" do
+      earlier_questions.each do |question|
+       expect(mail.body.encoded).to_not match question.title
+      end
     end
   end
 end
